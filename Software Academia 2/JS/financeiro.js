53a1c0162aa6102051af3a58fb6d4133ed01ad2a
JeanRibeiro2407 <Jeancr3211@gmail.com>
2023-11-28 08:42:43 -0300

    Financeiro.JS
    Alterado o Selecionar no financeiro

--- Software Academia 2/JS/financeiro.js
+++ Software Academia 2/JS/financeiro.js
@@ -4,23 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
         option.addEventListener('change', selecao);
     });
 
-   
     var selecionarBtn = document.getElementById('selecionar');
-    selecionarBtn.addEventListener('click', function () {
-        selecao(); 
-    });
+    selecionarBtn.addEventListener('click', selecao);
 });
 
 function selecao() {
-let pix = document.getElementById("pix");
-let boleto = document.getElementById("boleto");
-let cartaodecredito = document.getElementById("cartaodecredito");
+    let pix = document.getElementById("pix");
+    let boleto = document.getElementById("boleto");
+    let cartaodecredito = document.getElementById("cartaodecredito");
 
-pix.addEventListener('change', selecao);
-boleto.addEventListener('change', selecao);
-cartaodecredito.addEventListener('change', selecao);
-
-function selecao() {
     let div_pix = document.getElementById("div-pix");
     let div_boleto = document.getElementById("div-boleto");
     let div_cartaodecredito = document.getElementById("div-cartaoDeCredito");
@@ -29,18 +21,14 @@ function selecao() {
         div_pix.style.display = "inline";
         div_boleto.style.display = "none";
         div_cartaodecredito.style.display = "none";
-    }
-    if (boleto.checked) {
+    } else if (boleto.checked) {
         div_pix.style.display = "none";
         div_boleto.style.display = "inline";
         div_cartaodecredito.style.display = "none";
-    }
-    if (cartaodecredito.checked) {
+    } else if (cartaodecredito.checked) {
         document.getElementById("cartao-title").style.display = "block";
         div_pix.style.display = "none";
         div_boleto.style.display = "none";
         div_cartaodecredito.style.display = "grid";
     }
 }
-}
-
